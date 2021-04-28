22a79234e9360a5aa13e7a28f68f400bda957022
Carlos De Vera <c952d843@ku.edu>
2021-04-28 11:20:25 -0500

    Added Exercises 1 and 2.

---
+++ CreateUser.php
@@ -0,0 +1,39 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu","carlosdevera","ois3Phai","carlosdevera");
+$newUser = $_POST["user"];
+$present = FALSE;
+echo "<p>".$newUser. "</p>";
+if($mysqli->connect_errno){
+     echo "<p>Connection Failed</p>";
+     exit();
+}
+
+$query = "SELECT user_id FROM Users";
+
+if ($newUser != ""){
+    if($result = $mysqli->query($query)){
+        while($row = $result->fetch_assoc()){
+            if ($newUser == $row["user_id"]){
+                $present = TRUE;
+            }
+        }
+
+        $result->free();
+    }
+    if(!$present){
+        $query = "INSERT INTO Users (user_id)
+        VALUES ('$newUser')";
+     
+        if($mysqli->query($query) ==TRUE){
+            echo "<p>Added New UserName</p>";
+        }
+        else{
+            echo "<p>New User Name not added</p>";
+        }
+     }
+}
+else{
+    echo "<p>Cannot have a blank username</p><br>";
+}
+
+?>
